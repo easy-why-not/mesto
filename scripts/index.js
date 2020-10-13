a5ec3fad6a5708d08e3501a5d0ecf135a13be789
AlexandrNistratov <koss.1991@yandex.ru>
2020-10-13 07:51:09 +0300

    Пытаюсь сделать отображение картинки по ссылке

--- scripts/index.js
+++ scripts/index.js
@@ -9,7 +9,6 @@ let jobInput = document.querySelector('.popup__input_type_career');
 let profileName = document.querySelector('.profile__info-name');
 let profileCareer = document.querySelector('.profile__info-text');
 
-console.log({popup, popupOpenButton, popupCloseButton});
 
 const popupOpen = function () {
   nameInput.value = profileName.textContent;
@@ -32,7 +31,7 @@ let formElement = document.querySelector('form')
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
 function formSubmitHandler (event) {
-    event.preventDefault();
+    event.preventDefault(); //отменяем отправку формы
     profileName.textContent = nameInput.value;
     profileCareer.textContent = jobInput.value;
     popupClose();
@@ -44,7 +43,7 @@ formElement.addEventListener('submit', formSubmitHandler);
 
 
 
-//Добавление картинок
+// Попап добавление картинок
 
 let popupAddImage = document.querySelector('.popup__add-image');
 let popupOpenAddImageButton = document.querySelector('.profile__add-button');
@@ -60,4 +59,100 @@ const popupAddClose = function () {
 popupOpenAddImageButton.addEventListener('click', popupAddOpen)
 popupCloseAdd.addEventListener('click', popupAddClose)
 
+
+// Добавление картинок
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+
+const elements = document.querySelector('.elements');
+const saveImageButton = document.querySelector('.popup__save-image-button');
+const inputName = document.querySelector('.popup__input_type_name');
+const inputLink = document.querySelector('.popup__input_type_link');
+const template = document.querySelector('.template');
+
+
+
+const renderCards = () =>  {
+  const items = initialCards.map(element => getItems(element));
+
+  elements.prepend(...items)
+};
+// Удаление карточки
+const handlerDelete = (event) => {
+  event.target.closest('.card').remove();
+};
+// Кнопка лайк
+const handlerLike = (event) => {
+  event.target.closest('.card__button').classList.toggle('card__button-like');
+
+};
+
+
+
+const getItems = (data) => {
+  const card = template.content.cloneNode(true);
+
+  card.querySelector('.card__title').innerText = data.name;
+  card.querySelector('.card__image').src = data.link;
+
+  const cardDel = card.querySelector('.card__del');
+  const buttonLike = card.querySelector('.card__button');
+
+  cardDel.addEventListener('click', handlerDelete);
+
+  buttonLike.addEventListener('click', handlerLike);
+
+  return card;
+
+};
+
+const saveImage = () => {
+  saveImageButton.addEventListener('click', (event) => {
+    event.preventDefault()
+    const item = getItems({
+      name: inputName.value
+
+     //link: inputLink.value
+
+    })
+    elements.prepend(item);
+    inputName.value = '';
+    inputLink.value = '';
+    popupAddClose();
+
+  });
+
+};
+
+renderCards();
+saveImage();
+
+
+//Открытие картинок
 
