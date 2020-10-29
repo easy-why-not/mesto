163daad928ef5ddfb267ccf2df51df3e05aa7e52
AlexandrNistratov <koss.1991@yandex.ru>
2020-10-29 16:57:41 +0300

    Исправил комменты ревьювера. Проектная работа 6.2

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,3 @@
-
-
 const popup = document.querySelector('.popup');
 const popupOpenButton = document.querySelector('.profile__edit-button');
 const popupCloseButton = document.querySelector('.popup__close');
@@ -14,6 +12,9 @@ const popupAddImage = document.querySelector('.popup__add-image');
 const popupOpenAddImageButton = document.querySelector('.profile__add-button');
 const popupCloseAdd = document.querySelector('.popup__close-add-image');
 
+const popupImage = document.querySelector('.popup__image');
+const popupText = document.querySelector('.popup__text');
+
 // Попап открытия картинок
 const  popupOpenCard = document.querySelector('.popup__open-card');
 const  popupCloseCard = document.querySelector('.popup__close-card');
@@ -25,89 +26,10 @@ const inputName = document.querySelector('.popup__input_type_name');
 const inputLink = document.querySelector('.popup__input_type_link');
 const template = document.querySelector('.template');
 
-
 // Находим форму в DOM
-const formElement = document.querySelector('form')
+const formElement = document.querySelector('form');
 
-// Универсальная функция открытия попапа
-const popupOpen = function (popups) {
-  popups.classList.add('popup_is-opened');
-};
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-function formSubmitHandler (event) {
-  event.preventDefault(); //отменяем отправку формы
-  profileName.textContent = nameInput.value;
-  profileCareer.textContent = jobInput.value;
-  popupClose(popup);
-}
-
-// Обработчик открытия попапа изменения информации
-popupOpenButton.addEventListener('click', function () {
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileCareer.textContent;
-  popupOpen(popup);
-});
-
-
-// Обработчик открытия попапа добавления изображения
-popupOpenAddImageButton.addEventListener('click', function (){
-  popupOpen(popupAddImage);
-});
-
-// Универсальная функция закрытия попапа
-const popupClose = function (popup) {
-  popup.classList.remove('popup_is-opened');
-};
-
-// Закрытие по клику
-document.addEventListener('click', function (evt){
-  popupClose(evt.target);
-});
-
-// Обработчик закрытия попапа информации
-popupCloseButton.addEventListener('click', function (){
-  popupClose(popup);
-});
-//закрытие попапа esc
-const escClose = function (evt, popup) {
-  if (evt.key === 'Escape') {
-  popupClose(popup);
-  }
-};
-//обработчик закрытия попапа  информации по esc
-document.addEventListener('keydown', function (evt){
-  escClose(evt, popup);
-});
-
-
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', formSubmitHandler);
-
-
-// Обработчик закрытия попапа добавления изображения
-popupCloseAdd.addEventListener('click', function (){
-  popupClose(popupAddImage);
-});
-
-// Обработчик закрытия попапа добавления изображения по esc
-document.addEventListener('keydown', function (evt){
-  escClose(evt, popupAddImage);
-});
-
-// Удаление карточки
-const handlerDelete = (event) => {
-  event.target.closest('.card').remove();
-};
-
-// Кнопка лайк
-const handlerLike = (event) => {
-  event.target.closest('.card__button').classList.toggle('card__button-like');
-};
-
-// Добавление картинок
+//Массив карточек
 const initialCards = [
   {
     name: 'Архыз',
@@ -134,6 +56,98 @@ const initialCards = [
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
 ];
+
+//Функции
+
+// Универсальная функция открытия попапа
+const popupOpen = function (popups) {
+  popups.classList.add('popup_is-opened');
+
+  //обработчик закрытия попапа по esc
+  document.addEventListener('keydown', escClose);
+};
+
+// Универсальная функция закрытия попапа
+const popupClose = function (popup) {
+  popup.classList.remove('popup_is-opened');
+
+  //снимаем обработчик закрытия попапа по esc
+  document.removeEventListener('keydown', escClose);
+};
+
+// Закрытие по клику
+const handlerOverlayClose = function (event) {
+  const popupTarget = event.target.closest('.popup');
+  if (event.target === popupTarget) {
+    popupClose(popupTarget);
+  }
+};
+
+//закрытие попапа esc
+const escClose = function (evt) {
+  const popupIsOpened = document.querySelector('.popup_is-opened');
+
+  if (evt.key === 'Escape') {
+    popupClose(popupIsOpened);
+  }
+};
+
+// Удаление карточки
+const handlerDelete = (event) => {
+  event.target.closest('.card').remove();
+};
+
+// Кнопка лайк
+const handlerLike = (event) => {
+  event.target.closest('.card__button').classList.toggle('card__button-like');
+};
+
+// Обработчики
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function formSubmitHandler (event) {
+  event.preventDefault(); //отменяем отправку формы
+  profileName.textContent = nameInput.value;
+  profileCareer.textContent = jobInput.value;
+  popupClose(popup);
+};
+
+// Обработчик открытия попапа изменения информации
+popupOpenButton.addEventListener('click', function () {
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileCareer.textContent;
+  popupOpen(popup);
+});
+
+// Обработчик открытия попапа добавления изображения
+popupOpenAddImageButton.addEventListener('click', function (){
+  popupOpen(popupAddImage);
+});
+
+
+//Обработчики закрытия по клику на оверлей
+popup.addEventListener('click', handlerOverlayClose);
+popupAddImage.addEventListener('click', handlerOverlayClose);
+popupOpenCard.addEventListener('click', handlerOverlayClose);
+
+// Обработчик закрытия попапа информации
+popupCloseButton.addEventListener('click', function (){
+  popupClose(popup);
+});
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', formSubmitHandler);
+
+
+// Обработчик закрытия попапа добавления изображения
+popupCloseAdd.addEventListener('click', function (){
+  popupClose(popupAddImage);
+});
+
+// Логика
+
 const elementsPrepend = function (a) {
   elements.prepend(...a);
 };
@@ -147,31 +161,26 @@ const renderCards = () =>  {
 const getItems = (data) => {
   const card = template.content.cloneNode(true);
 
-  const popupImage = document.querySelector('.popup__image');
-  const popupText = document.querySelector('.popup__text');
   const templateImage = card.querySelector('.card__image');
   const cardDel = card.querySelector('.card__del');
   const buttonLike = card.querySelector('.card__button');
 
   card.querySelector('.card__title').innerText = data.name;
-  card.querySelector('.card__image').src = data.link;
+  templateImage.src = data.link;
+  templateImage.alt = data.name;
 
   templateImage.addEventListener('click', function () {
     popupText.textContent = data.name;
     popupImage.src = data.link;
+    popupImage.alt = data.name;
 
     // Открываем попап изображения универсальной функцией
     popupOpen(popupOpenCard);
   });
 
-  // Закрываем попап изображения универсальной функцией
+  // Обработчик закрытия попапа изображения
   popupCloseCard.addEventListener('click', function (){
     popupClose(popupOpenCard);
-  });
-
-  // Закрываем попап изображения по esc
-  document.addEventListener('keydown', function (evt){
-    escClose(evt, popupOpenCard);
   });
 
   cardDel.addEventListener('click', handlerDelete);
