8fdd37bd8c817885249bf9819c82a508c93b25df
AlexandrNistratov <koss.1991@yandex.ru>
2020-12-08 12:10:58 +0300

    Проектная работа 8.1

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,6 +4,7 @@ import Section from "../components/Section.js";
 import PopupWithForm from '../components/PopupWithForm.js';
 import UserInfo from '../components/UserInfo.js';
 import PopupWithImage from "../components/PopupWithImage.js";
+import '../pages/index.css';
 
 //Массив карточек
 export const initialCards = [
